ca36d3ea33703ad123382c869bd0d3a8561aa7bb
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-12-25 17:19:25 +0300

    skill

---
+++ Proman.WebUI/ViewComponents/DefaultViewCompanenets/_DefaultSkillComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Proman.WebUI.DTOs.SkillDTOs;
+
+namespace Proman.WebUI.ViewComponents.DefaultViewCompanenets
+{
+    public class _DefaultSkillComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultSkillComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7081/api/Skills/GetLast6ActiveSkills");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var skills = JsonConvert.DeserializeObject<List<ResultSkillDTO>>(jsonData);
+                return View(skills);
+            }
+            return View();
+        }
+    }
+}
